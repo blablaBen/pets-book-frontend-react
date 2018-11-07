cd94eadca878fdbec6400a9b9ffcc61a43350776
Pattarasai Markpeng <pattarasaimarkpeng@Pattarasais-MacBook-Pro.local>
2018-11-07 15:33:06 -0800

    update

--- src/Feed/Feed.js
+++ src/Feed/Feed.js
@@ -14,28 +14,14 @@ const FeedContainer = styled.div.attrs({
 class Feed extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            postItems: []
-        };
     }
 
     componentWillMount() {
-        this.setState({postItems: [{
-            "commentCount": 0,
-            "date": "2018-11-06 12:54AM",
-            "id": "5be156cd964ac4092a64f6db",
-            "pictures": [
-                "https://i.kinja-img.com/gawker-media/image/upload/s--WFkXeene--/c_scale,f_auto,fl_progressive,q_80,w_800/ol9ceoqxidudap8owlwn.jpg"
-            ],
-            "textValue": "Hi!, this is my first status",
-            "userId": "5bde471d69c4f1059d4f5365"
-        }]})
         this.props.loadFeed();
     }
 
     render() {
         const {posts} = this.props;
-        console.log(posts);
         return (
             <FeedContainer>
                 <div className="col-8">
