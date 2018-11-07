65b23b20aecd2179e7ba20de7b0962aef3d9f32a
Pattarasai Markpeng <pattarasaimarkpeng@Pattarasais-MacBook-Pro.local>
2018-11-07 15:55:13 -0800

    Updates

---
+++ precache-manifest.bde24a7c99a62e8f9da578a420053e7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "9f885bcaab99e2a27a3c",
+    "url": "./static/js/main.9f885bca.chunk.js"
+  },
+  {
+    "revision": "c08510cea15104fb9f63",
+    "url": "./static/js/1.c08510ce.chunk.js"
+  },
+  {
+    "revision": "9f885bcaab99e2a27a3c",
+    "url": "./static/css/main.8ff3cb57.chunk.css"
+  },
+  {
+    "revision": "c08510cea15104fb9f63",
+    "url": "./static/css/1.ba0bd6f8.chunk.css"
+  },
+  {
+    "revision": "bfebe547b84cb0976c32192ce148ef7b",
+    "url": "./index.html"
+  }
+];
