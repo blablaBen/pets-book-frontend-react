5a25b96a3c5f327409c3a5e75e1d66ed2735ef96
Pattarasai Markpeng <pattarasaimarkpeng@Pattarasais-MacBook-Pro.local>
2018-12-02 12:10:49 -0800

    decorate new-post

--- src/Feed/Components/NewPost.js
+++ src/Feed/Components/NewPost.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import PostContainer from './PostContainer';
-
+import { Button } from 'reactstrap';
 
 const AvatarImgStyle = {
     borderRadius: '50%',
@@ -26,6 +26,14 @@ const PostTextAreaStyle = {
     border: '0px solid rgb(224, 220, 220)'
 }
 
+const UpperContainer = styled.div.attrs({
+    className: "col-12"
+})`
+    border-bottom: 1px solid #dedede;
+    margin-bottom: 15px;
+    padding-bottom: 15px;
+`;
+
 export default class NewPost extends Component {
 
     constructor(props) {
@@ -39,22 +47,37 @@ export default class NewPost extends Component {
         this.setState({textarea : value});
     };
 
+    onClickSubmit = () => {
+        const {textArea} = this.state;
+        this.onClickSubmit({content : textArea});
+    }
+
     render() {
         const { onClickSubmit, portraitUrl } = this.props;
         const {textarea} = this.state;
         return (
             <PostContainer>
                 <div className="row">
-                    <div className="col-2">
-                        <AvatarComponent url={portraitUrl} ></AvatarComponent>
-                    </div>
-                    <div className="col-8">
-                        <textArea className="col-12" style={PostTextAreaStyle} placeholder="What's on your mind?" onChange={
-                            e => {
-                                this.onTextAreaChanged(e.target.value)
-                            }
-                        }></textArea>
-                    </div>
+                    <UpperContainer>
+                        <div className="row">
+                            <div className="col-2">
+                                <AvatarComponent url={portraitUrl} ></AvatarComponent>
+                            </div>
+                            <div className="col-8">
+                                <textArea className="col-12" style={PostTextAreaStyle} placeholder="What's on your mind?" onChange={
+                                    e => {
+                                        this.onTextAreaChanged(e.target.value)
+                                    }
+                                }></textArea>
+                            </div>
+                        </div>
+                    </UpperContainer>
+                </div>
+                <div className="row">
+                        <div className="col-10"></div>
+                        <div className="col-2">
+                            <Button outline color="primary" block onClick={this.onLogIn}>Post</Button>{' '}
+                        </div> 
                 </div>
             </PostContainer>
         );
