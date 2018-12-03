034187e18fec9581814d4819fc7e7ec32c4fefff
Pattarasai Markpeng <pattarasaimarkpeng@Pattarasais-MacBook-Pro.local>
2018-12-03 11:45:29 -0800

    update homepage

--- src/Const/URLConstant.js
+++ src/Const/URLConstant.js
@@ -1,5 +1,4 @@
-export const HOST = "http://localhost:8080/appbase";
-
+export const HOST = "http://lili.app2018.live:8080/appbase";
 export const FEED_URL = "/afterLogin/feed"
 export const INTEREST_URL = "/afterLogin/interest"
 export const ASK_URL = "/afterLogin/ask"
